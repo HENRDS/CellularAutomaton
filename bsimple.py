67beec4060a55980eb252b905f56a56732a465e7
Henry R. Da Silva <henryqx@hotmail.com>
2018-03-27 18:56:32 -0300

    Boolean simplifier improved

--- bsimple.py
+++ bsimple.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-
+import os
 import tkinter as tk
 from sympy import *
+
 
 class MainWindow(tk.Tk):
     def __init__(self, vrs, res, ready, master=None):
         tk.Tk.__init__(self, master)
         self.ready = ready
         self.bvars = vrs
-        self.res = res#{ name : [] for name in res}
         self.canvas = tk.Canvas(self)
+        self.main_panel = tk.Frame(self.canvas)
+        if ready:
+            self.res = {}
+            for k, v in res.items():
+                self.res[k] = [tk.BooleanVar(master=self.main_panel, value=b) for b in v]
+        else:
+            self.res = res
         self.btn = tk.Button(self.canvas, command=self.process, text='process')
         self.btn.grid(sticky=tk.NSEW, column=0, row=0)
         self.draw()
@@ -28,25 +35,19 @@ class MainWindow(tk.Tk):
             print(SOPform(syms, prods))
 
     def build_lhs(self):
-        fmt = ' '.join([f'{{args[{i}]:{len(v)}}}' for i, v in  enumerate(self.bvars)])
+        fmt = ' '.join([f'{{args[{i}]:{len(v)}}}' for i, v in enumerate(self.bvars)])
         htxt = fmt.format(args=self.bvars)
         h = tk.Label(self.main_panel, text=htxt)
-        h.grid(column=0, columnspan=self.nvars, row=0,sticky=tk.NSEW)
+        h.grid(column=0, columnspan=self.nvars, row=0, sticky=tk.NSEW)
         for i in range(0, 2 ** self.nvars):
-            l = tk.Label(self.main_panel, text=fmt.format(
-                args=list(format(i, f'0{self.nvars}b'))))
-            l.grid(column=0, columnspan=self.nvars, row=i+1, sticky=tk.NSEW)
+            txt = fmt.format(args=list(format(i, f'0{self.nvars}b')))
+            print(txt)
+            l = tk.Label(self.main_panel, text=txt)
+            l.grid(column=0, columnspan=self.nvars, row=i + 1, sticky=tk.NSEW)
         self.lhs = h
 
     def draw(self):
         self.nvars = len(self.bvars)
-        self.main_panel = tk.Frame(self.canvas)
-        # var headers
-        # h = tk.Label(self.main_panel, text=' '.join(self.bvars))
-        # h.grid(column=0, columnspan=self.nvars, row=0, sticky=tk.NSEW)
-        # for i, v in enumerate(self.bvars):
-        #     t = tk.Label(self.main_panel, text=v)
-        #     t.grid(column=i, columnspan=1, row=0, sticky=tk.NSEW)
         tk.Label(self.main_panel, text=' |=| ').grid(
             column=self.nvars, columnspan=1, row=0, sticky=tk.NSEW)
         # Res headers
@@ -55,17 +56,12 @@ class MainWindow(tk.Tk):
             t.grid(column=i + self.nvars + 1, columnspan=1, row=0, sticky=tk.NSEW)
         self.build_lhs()
         for i in range(0, 2 ** self.nvars):
-            # c = tk.Label(self.main_panel, text=' '.join(list(format(i, f?'0{self.nvars}b'))))
-            # c.grid(column=0, columnspan=self.nvars, row=i+1, sticky=tk.NSEW)
-            # for n , v in enumerate(self.bvars):
-            #     v = self.true_im if (i & (1 << (self.nvars - (n+1)))) > 0 else self.false_im
-            #     c = tk.Label(self.main_panel, image=v)
-            #     c.grid(column=n, columnspan=1, row=i+1, sticky=tk.NSEW)
+            self.refs = []
             for n, r in enumerate(self.res.keys()):
-                vv = tk.BooleanVar(self, value=False)
-                c = tk.Checkbutton(self.main_panel, variable=vv)
-                self.res[r].append(vv)
-                c.grid(column=self.nvars+1+n, row=i+1, sticky=tk.NSEW)
+                c = tk.Checkbutton(self.main_panel, variable=self.res[r][i] if self.ready else tk.BooleanVar(value=
+                                                                                                           False))
+                c.grid(column=self.nvars + 1 + n, row=i + 1, sticky=tk.NSEW)
+                self.refs.append(c)
         sbar = tk.Scrollbar(self, command=self.canvas.yview)
         self.canvas.scrollbar = sbar
         sbar.grid(column=1, sticky=tk.NSEW)
@@ -74,12 +70,18 @@ class MainWindow(tk.Tk):
         self.canvas.config(yscrollcommand=sbar.set)
 
     def on_close(self):
-        data = { k : [b.get() for b in v] for k,v in self.res.items()}
+        data = {k: [b.get() for b in v] for k, v in self.res.items()}
         with open('last', 'w') as f:
             f.write(str(data))
         self.destroy()
 
-v = ['n', 's', 'e', 'w', 'nw', 'ne', 'sw', 'se'] 
-# w = MainWindow(['a', 'b'], ['r'])
-w = MainWindow(v,{x : [] for x in v}, False)
+
+v = ['n', 's', 'e', 'w', 'nw', 'ne', 'sw', 'se']
+res = {x: [] for x in v}
+r = False
+if os.path.isfile('last'):
+    print('found')
+    res = eval(open('last', 'r').read())
+    r = True
+w = MainWindow(v, res, r)
 w.mainloop()
